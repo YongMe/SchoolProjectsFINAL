234b91929a3465dd4ff19a4886d16f282caa661c
YongMe <92907633+YongMe@users.noreply.github.com>
2022-11-06 13:16:14 +0530

    Update SixthProgram.java

--- SixthProgram.java
+++ SixthProgram.java
@@ -6,6 +6,6 @@ public class SixthProgram {
         String s1 = sc.nextLine();
         System.out.print("Enter second string: ");
         String s2 = sc.nextLine();
-        System.out.println("Comparison of both string lexicographically: " + s1.compareTo(s2));
+        System.out.println("Result of comparison of both string lexicographically: " + s1.compareTo(s2));
     }
 }
