c710fdd50c9f428e93f00233d20cc116366b2ce3
YongMe <92907633+YongMe@users.noreply.github.com>
2022-11-06 13:13:09 +0530

    Update FifthProgram.java

--- FifthProgram.java
+++ FifthProgram.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class FifthProgram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the string: ");
+        System.out.print("Enter the word: ");
         String s = sc.next();
         s = s.replaceAll("[aeiouAEIOU]", "");
-        System.out.println("New String: " + s);
+        System.out.println("New word: " + s);
     }
 }
