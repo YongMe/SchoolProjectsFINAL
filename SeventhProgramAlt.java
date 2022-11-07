376406caa30ee39240a172b9b10fa2a682a3a93c
YongMe <92907633+YongMe@users.noreply.github.com>
2022-11-07 17:23:18 +0530

    Create SeventhProgramAlt.java

---
+++ SeventhProgramAlt.java
@@ -0,0 +1,32 @@
+import java.io.*;
+public class questionFIVE2008
+{
+    public static void main(String args[]) throws IOException
+    {
+        int ch;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter a string: ");
+        String input=br.readLine();
+        int len=input.length();
+        int i;
+        System.out.print("New String: ");
+        for(i=0; i<len; i++)
+        {
+            ch=input.charAt(i);
+            if(ch>=65 && ch<=90)
+            {
+                ch=ch+32;
+                System.out.print((char)ch);
+            }
+            else if(ch>=97 && ch<=122)
+            {
+                ch=ch-32;
+                System.out.print((char)ch);
+            }
+            else
+            {
+                System.out.print((char)ch);
+            }
+        }
+    }
+}
