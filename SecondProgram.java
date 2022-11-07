511644c05b9486456252e5212fc9b10e1f53531a
YongMe <92907633+YongMe@users.noreply.github.com>
2022-11-07 19:58:11 +0530

    Update SecondProgram.java

--- SecondProgram.java
+++ SecondProgram.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SecondProgram {
+public class Grading {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double[] ar = new double[10];
