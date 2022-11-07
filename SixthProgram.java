c6f858f5e331d8a8b818901882b80a3150967382
YongMe <92907633+YongMe@users.noreply.github.com>
2022-11-07 22:54:48 +0530

    Update SixthProgram.java

--- SixthProgram.java
+++ SixthProgram.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class SixthProgram {
+public class LexicographicalComparison {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first string: ");
