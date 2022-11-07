fadfb497c7dafa43c7bb6cfe8eb9127d0cd617b9
YongMe <92907633+YongMe@users.noreply.github.com>
2022-11-07 21:47:26 +0530

    Update ThirdProgram.java

--- ThirdProgram.java
+++ ThirdProgram.java
@@ -5,25 +5,25 @@ public class ElementRemoval
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int n;    //Array Size Declaration
+        int n,i,j;    //Array Size Declaration
         System.out.print("Enter the number of elements: ");
         n=sc.nextInt();    //Array Size Initialization
 
         int arr[]=new int[n];    //Array Declaration
         System.out.println("Enter the elements of the array :");
-        for(int i=0;i<n;i++)     //Array Initialization
+        for(i=0;i<n;i++)     //Array Initialization
         {
             arr[i]=sc.nextInt();
         }
         System.out.print("Enter the index of the element you want to remove ");
         int index = sc.nextInt();
 
-        for(int i = 0; i < arr.length; i++)
+        for(i = 0; i < arr.length; i++)
         {
             if(i == index)   //If element found
             {
                 // shifting elements
-                for(int j = i; j < arr.length - 1; j++)
+                for(j = i; j < arr.length - 1; j++)
                 {
                     arr[j] = arr[j+1];
                 }
@@ -32,7 +32,7 @@ public class ElementRemoval
         }
 
         System.out.println("Elements after deletion " );
-        for(int i = 0; i < arr.length-1; i++)
+        for(i = 0; i < arr.length-1; i++)
         {
             System.out.print(arr[i]+ " ");
         }
