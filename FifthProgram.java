c5ba7768cee90b9392af636d9627383354231c5e
YongMe <92907633+YongMe@users.noreply.github.com>
2022-11-07 22:49:48 +0530

    Update FifthProgram.java

--- FifthProgram.java
+++ FifthProgram.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class FifthProgram {
+public class VowelRemoval {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the word: ");
